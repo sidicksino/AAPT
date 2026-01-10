52420e2e3ed4b7ad8683a11692f765cd387ff1ae
SidickSino <sidickabdoulayesino1@gmail.com>
2026-01-10 12:35:46 +0200

    Add more locally news 2

--- src/data/localNews.ts
+++ src/data/localNews.ts
@@ -51,7 +51,7 @@ export const localNews: NewsArticle[] = [
         title: "Renforcement des activités de l’AAPT lors du Conseil du 14 décembre 2024",
         excerpt: "Le samedi 14 décembre 2024, au sein de l’Association Actions Pour Tous (AAPT), plusieurs points ont été abordés concernant le renforcement des activités à venir. Les discussions ont été constructives, et les membres du bureau exécutif ont réaffirmé leur détermination à intensifier leurs actions afin de générer un impact positif sur la communauté. Cette réunion illustre l’engagement de l’AAPT à améliorer la cohésion interne et à préparer des initiatives efficaces pour le développement social et communautaire.",
         date: "Samedi 14 décembre 2024",
-        category: "cohesion",
+        category: "social_cohesion",
         image: "/assets/local_images/5.jpg",
         linkKey: "more",
         created_at: "2026-01-10T10:00:05Z",
@@ -99,6 +99,72 @@ export const localNews: NewsArticle[] = [
         image: "/assets/local_images/9.jpg",
         linkKey: "more",
         created_at: "2026-01-10T10:00:09Z",
+        type: "Article"
+    },
+    {
+        id: -10,
+        title: "Initiative solidaire IFTAR SAYIM de l’AAPT au rond-point du Palais du 15 Janvier",
+        excerpt: "Le vendredi 07 mars 2025, l’Association Actions Pour Tous (AAPT) a lancé une initiative solidaire intitulée IFTAR SAYIM au rond-point du Palais du 15 Janvier. À cette occasion, l’équipe de l’AAPT a procédé à la distribution de dattes et d’eau aux jeûneurs bloqués dans la circulation au moment de l’appel du Muazzin, leur permettant ainsi de rompre le jeûne dans la dignité. Cette action humanitaire illustre l’esprit de solidarité et de partage de l’Association Actions Pour Tous, toujours engagée à soutenir les populations en situation de besoin, particulièrement durant le mois sacré de Ramadan.",
+        date: "Vendredi 07 mars 2025",
+        category: "solidarity",
+        image: "/assets/local_images/10.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:10Z",
+        type: "Article"
+    },
+    {
+        id: -11,
+        title: "Distribution de vêtements aux élèves orphelins de l’orphelinat SOS de N’Djari",
+        excerpt: "Le mardi 25 mars 2025, l’Association Actions Pour Tous (AAPT), en collaboration avec l’initiative IFTAR SAWA, a organisé une distribution de vêtements au profit des élèves orphelins de l’orphelinat SOS, situé dans le quartier N’Djari. Cette action a été une occasion forte de manifester la solidarité et l’engagement de l’association envers les enfants les plus vulnérables. Grâce à cette initiative, plusieurs élèves ont pu bénéficier d’un soutien concret et réconfortant. L’Association Actions Pour Tous adresse ses remerciements sincères à tous les contributeurs et partenaires qui ont permis la réussite de cette action et offert un moment de joie et de dignité à ce",
+        date: "Mardi 25 mars 2025",
+        category: "solidarity",
+        image: "/assets/local_images/11.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:11Z",
+        type: "Article"
+    },
+    {
+        id: -12,
+        title: "Visite de compassion au Centre des Personnes Handicapées de Diguel – Initiative « Une Main, Une Âme »",
+        excerpt: "Le samedi 26 avril 2025, l’Association Actions Pour Tous (AAPT), à travers son initiative « Une Main, Une Âme », a effectué une visite de compassion au Centre des Personnes Handicapées, situé au quartier Diguel, dans le 8ᵉ arrondissement de N’Djamena. Cette visite a été une occasion de manifester la solidarité et l’engagement de l’association envers les personnes handicapées et leurs familles.",
+        date: "Samedi 26 avril 2025",
+        category: "solidarity",
+        image: "/assets/local_images/12.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:12Z",
+        type: "Article"
+    },
+    {
+        id: -13,
+        title: "Audience avec le Secrétaire Permanent de la CNARR",
+        excerpt: "Le 29 juin 2024, l’équipe de l’Association Actions Pour Tous (AAPT) a été reçue par M. Idriss Mahamat Ali Abdallah Nassour, Secrétaire Permanent de la Commission Nationale d’Accueil des Réfugiés et Rapatriés (CNARR). Lors de cette audience, l’association a présenté ses objectifs, missions et activités, tout en exprimant sa disponibilité à intervenir comme volontaire auprès des réfugiés à N’Djamena et dans l’est du pays, notamment à travers des actions de sensibilisation. Le Secrétaire Permanent a salué l’engagement de l’association et encouragé la poursuite de ces actions en faveur des personnes vulnérables. La rencontre s’est conclue par une photo de famille, marquant un esprit de collaboration et de solidarité.",
+        date: "29 juin 2024",
+        category: "social_cohesion",
+        image: "/assets/local_images/13.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:13Z",
+        type: "Article"
+    },
+    {
+        id: -14,
+        title: "Conférence-débat sur l’emploi des jeunes : entre obstacles et désespoir",
+        excerpt: "Le mardi 18 mars 2025, l’Association Actions Pour Tous (AAPT) a organisé une conférence-débat sur le thème « L’emploi des jeunes : entre obstacles et désespoir ». Cette rencontre a permis des échanges riches et constructifs, visant à mieux comprendre les difficultés rencontrées par les jeunes et à explorer des pistes de solutions concrètes. L’association remercie l’ensemble des participants pour leur engagement et leur contribution active.",
+        date: "Mardi 18 mars 2025",
+        category: "society",
+        image: "/assets/local_images/14.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:14Z",
+        type: "Article"
+    },
+    {
+        id: -15,
+        title: "Distribution de kits scolaires au profit des élèves",
+        excerpt: "Le 23 octobre 2024, l’Association Actions Pour Tous (AAPT) a mené une action de distribution de kits scolaires en faveur des élèves, afin de soutenir leur parcours éducatif et d’alléger les charges des familles. Cette initiative traduit l’engagement de l’association pour l’éducation et l’égalité des chances, en apportant une aide concrète aux enfants en situation de vulnérabilité.",
+        date: "23 octobre 2024",
+        category: "solidarity",
+        image: "/assets/local_images/15.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:15Z",
         type: "Article"
     }
 ];
