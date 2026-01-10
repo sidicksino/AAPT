5428d62ed8212c629ff5995a0cd4b51dcfd5c369
SidickSino <sidickabdoulayesino1@gmail.com>
2026-01-10 11:49:15 +0200

    Add news locally

--- src/data/localNews.ts
+++ src/data/localNews.ts
@@ -1,3 +1,4 @@
+import { date } from 'react-i18next/icu.macro';
 import { NewsArticle } from '../types';
 
 export const localNews: NewsArticle[] = [
@@ -15,23 +16,89 @@ export const localNews: NewsArticle[] = [
     {
         id: -2,
         title: "Distribution de fournitures scolaires par l’AAPT",
-        excerpt: "Retour sur les moments forts de notre conférence annuelle dédiée à l'accès aux soins pour les communautés isolées.",
-        date: "2023-11-20",
-        category: "health",
-        image: "https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?q=80&w=2070&auto=format&fit=crop",
+        excerpt: "Le jeudi 10 octobre 2024, l’équipe de l’Association Actions Pour Tous (AAPT) a mené une journée gratifiante sur le terrain en distribuant des fournitures scolaires aux élèves de CPI à CM2. Cet événement a été un moment précieux de partage et de soutien concret pour les enfants, illustrant l’engagement de l’AAPT à contribuer activement à l’éducation des jeunes. L’association tient à remercier tous ceux qui ont rendu cette action possible, et se réjouit de continuer à œuvrer pour le bien-être et le développement des communautés tchadiennes.",
+        date: "Jeudi 10 octobre 2024",
+        category: "solidarity",
+        image: "/assets/local_images/2.jpg",
         linkKey: "more",
         created_at: "2026-01-10T10:00:02Z",
         type: "Article"
     },
     {
         id: -3,
-        title: "Atelier de formation des bénévoles",
-        excerpt: "Plus de 50 bénévoles ont participé à notre week-end de formation intensive pour préparer les actions d'été.",
-        date: "2023-06-10",
-        category: "solidarity",
-        image: "https://images.unsplash.com/photo-1559027615-cd4628902d4a?q=80&w=2074&auto=format&fit=crop",
+        title: "Signature d’un partenariat entre l’AAPT et Amina-Security Informatique",
+        excerpt: "Le lundi 30 septembre 2024, l’Association Actions Pour Tous (AAPT) a signé une convention de partenariat avec Amina-Security Informatique. Cette collaboration a pour objectif de promouvoir le bien-être de la communauté en renforçant la sécurité informatique et en favorisant l’accès à des solutions numériques adaptées à tous. En travaillant ensemble, l’AAPT et Amina-Security s’engagent à offrir des solutions concrètes et adaptées aux besoins des communautés tchadiennes, contribuant ainsi au développement et à la protection numérique au sein de la société.",
+        date: "Lundi 30 septembre 2024",
+        category: "society",
+        image: "/assets/local_images/3.jpg",
         linkKey: "more",
         created_at: "2026-01-10T10:00:03Z",
+        type: "Article"
+    },
+    {
+        id: -4,
+        title: "Signature d’un partenariat entre l’AAPT et CEFPAMS Academy",
+        excerpt: "Le lundi 9 septembre 2024, l’Association Actions Pour Tous (AAPT) a eu le plaisir de formaliser un partenariat avec CEFPAMS Academy, marquant ainsi un engagement commun vers des objectifs partagés. Cette collaboration vise à promouvoir l’accès à l’éducation, renforcer les compétences professionnelles et contribuer à l’inclusion sociale et à l’égalité des chances pour les populations défavorisées au Tchad. Ensemble, l’AAPT et CEFPAMS Academy mettront en œuvre des formations innovantes et des initiatives concrètes, tout en sensibilisant la communauté aux enjeux éducatifs et sociaux. L’association réaffirme également sa volonté de collaborer avec toutes les organisations de la société civile souhaitant s’associer à ces efforts pour atteindre des objectifs communs et générer un impact significatif au sein de la société.",
+        date: "Lundi 9 septembre 2024",
+        category: "society",
+        image: "/assets/local_images/4.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:04Z",
+        type: "Article"
+    },
+    {
+        id: -5,
+        title: "Renforcement des activités de l’AAPT lors du Conseil du 14 décembre 2024",
+        excerpt: "Le samedi 14 décembre 2024, au sein de l’Association Actions Pour Tous (AAPT), plusieurs points ont été abordés concernant le renforcement des activités à venir. Les discussions ont été constructives, et les membres du bureau exécutif ont réaffirmé leur détermination à intensifier leurs actions afin de générer un impact positif sur la communauté. Cette réunion illustre l’engagement de l’AAPT à améliorer la cohésion interne et à préparer des initiatives efficaces pour le développement social et communautaire.",
+        date: "Samedi 14 décembre 2024",
+        category: "cohesion",
+        image: "/assets/local_images/5.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:05Z",
+        type: "Article"
+    },
+    {
+        id: -6,
+        title: "Formation sur le montage de projets humanitaires par l’AAPT",
+        excerpt: "Le mardi 10 septembre 2024, l’Association Actions Pour Tous (AAPT) a animé une formation dédiée au montage de projets humanitaires. Les participants ont pu explorer les étapes essentielles, de la conception à la mise en œuvre d’un projet, leur permettant d’apprendre à structurer et planifier des initiatives impactantes pour les communautés en difficulté. Cette initiative souligne l’engagement de l’AAPT à former et soutenir les acteurs du secteur humanitaire, contribuant ainsi à un développement durable et solidaire des communautés tchadiennes.",
+        date: "Mardi 10 septembre 2024",
+        category: "event",
+        image: "/assets/local_images/6.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:06Z",
+        type: "Article"
+    },
+    {
+        id: -7,
+        title: "Mission humanitaire de l’AAPT auprès des réfugiés à Kalambari",
+        excerpt: "Le vendredi 20 septembre 2024, l’Association Actions Pour Tous (AAPT) a mené une mission confiée par la Commission Nationale d’Accueil et de Réinsertion des Réfugiés et Rapatriés (CNARR), sous l’autorisation du Secrétaire Permanent, M. Idriss Mahamat Ali Abdallah Nassour. Malgré les conditions météorologiques défavorables, la distance à parcourir et les inondations, les membres dévoués de l’AAPT ont persévéré pour atteindre le camp de réfugiés de Kalambari, parfois même en utilisant des pirogues pour se frayer un chemin. Cette mission consistait à identifier les besoins des réfugiés afin d’améliorer leurs conditions de vie. La détermination et l’altruisme de l’AAPT démontrent son engagement concret à venir en aide aux populations vulnérables, et leur action mérite admiration et respect.",
+        date: "Vendredi 20 septembre 2024",
+        category: "solidarity",
+        image: "/assets/local_images/7.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:07Z",
+        type: "Article"
+    },
+    {
+        id: -8,
+        title: "Rencontre de l’AAPT avec le Directeur Délégué de l’Institut Français du Tchad",
+        excerpt: "Le jeudi 20 février 2025, l’Association Actions Pour Tous (AAPT) a eu l’honneur de rencontrer le Directeur Délégué de l’Institut Français du Tchad. Cette rencontre a été enrichissante et fructueuse, ouvrant la voie à de belles collaborations à venir entre l’Institut Français et l’AAPT, dans le but de promouvoir l’éducation, la culture et le développement communautaire.",
+        date: "Jeudi 20 février 2025",
+        category: "society",
+        image: "/assets/local_images/8.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:08Z",
+        type: "Article"
+    },
+    {
+        id: -9,
+        title: "Distribution de kits aux nouveau-nés par l’AAPT à l’Hôpital Tchad-Chine",
+        excerpt: "Le mercredi 1er janvier 2025, l’Association Actions Pour Tous (AAPT) a entamé l’année en se rendant à l’hôpital Tchad-Chine pour distribuer des kits savonniers et des couches aux bébés nouveau-nés. Cette initiative illustre l’engagement de l’AAPT envers les familles démunies et sa volonté d’apporter un soutien concret aux plus vulnérables. L’association félicite toute l’équipe pour cette action qui annonce une année 2025 placée sous le signe de la solidarité et de la générosité.",
+        date: "Mercredi 1er janvier 2025",
+        category: "health",
+        image: "/assets/local_images/9.jpg",
+        linkKey: "more",
+        created_at: "2026-01-10T10:00:09Z",
         type: "Article"
     }
 ];
